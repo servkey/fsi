6b97c121d9a25401edddd71a7338c0b4df6cf662
servkey <luis.montane@hotmail.com>
2014-02-13 15:50:22 -0600

    Lab 1 – Servidor que responda información - Código

---
+++ labs/lab01-cliente_servidor/Server.java
@@ -0,0 +1,38 @@
+import java.net.*;
+import java.io.*;
+import java.lang.management.*;
+public class Server{
+
+	public static void main(String args[]){
+		try {
+
+			int puerto = 5000;
+
+			ServerSocket s = new ServerSocket(puerto);
+			String comandoSalir = "Exit";
+			String entrada = "";
+			System.out.println("Servidor iniciado en el puerto " + puerto + "...");
+			while(true){
+				Socket s1 = s.accept();        
+				System.out.println("Aceptando conexion...");
+				PrintWriter out = new PrintWriter(s1.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(s1.getInputStream()));
+
+				while ((entrada = in.readLine()) != null) {					
+				    System.out.println(entrada);
+				    out.println("Hola desde el servidor!!");
+				    out.println("");
+				    s1.close();
+				    if (entrada.trim().equals(comandoSalir))
+				        return;
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
+
+}
