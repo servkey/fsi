20a7644999c99cec1f4d3546c22530bc9baa631e
servkey <luis.montane@hotmail.com>
2016-08-07 13:36:57 -0500

    Línea Base.

--- labs/corba/server/startServer.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Iniciar el servidor
-REM Luis G. Montané Jiménez 
-
-SET CLASSPATH=%CLASSPATH%;../idl/Hola.jar;../idl/Hola.jar;../../Recursos/ConexionMiniFramework/frameworkmini.jar;
-echo ********************************************************
-echo Ejecutando servidor!!!!!
-java HolaServer -ORBInitialPort 2000 -ORBInitialHost localhost
--- labs/corba/server/startService.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Iniciar el servicio orbd
-REM Luis G. Montané Jiménez 
-
-start orbd -ORBInitialPort 2000
