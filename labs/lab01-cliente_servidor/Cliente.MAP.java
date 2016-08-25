fd302aa0bd22f2afa9a1f61d05e1ec9795f79d85
servkey <luis.montane@hotmail.com>
2016-08-25 10:58:25 -0500

    Cliente

---
+++ labs/lab01-cliente_servidor/Cliente.MAP.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+public class ClienteMAP{
+    public static void main(String[] args) {
+	//java ClienteMAP 192.168.11.19 1 29
+	System.out.println("IP: " + args[0]);
+	System.out.println("Puerto inicial: " + args[1]);
+	System.out.println("Puerto final: " + args[2]);
+
+      String servidor = args[0];
+      int puerto = 5302;
+
+	int puerto_inicial = Integer.parseInt(args[1]);
+	int puerto_final = Integer.parseInt(args[2]);
+
+      try{
+	System.out.println("Antes del socket");
+        Socket socket= new Socket (servidor,puerto);
+	System.out.println("Después del socket");
+	socket.close();
+      } catch (IOException e)
+      {
+       		System.out.println("Error en conexión al intentar con el puerto " + puerto  + "!!!");
+      }
+      
+    }
+}
