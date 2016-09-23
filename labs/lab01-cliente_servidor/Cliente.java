1ab8cafcb6586020b4cd7b49bd936ec21eccd781
servkey <luis.montane@hotmail.com>
2016-09-23 17:13:23 -0500

    Ejemplo Chat.

--- labs/lab01-cliente_servidor/Cliente.java
+++ labs/lab01-cliente_servidor/Cliente.java
@@ -3,24 +3,24 @@ import java.net.*;
 
 public class Cliente{
     public static void main(String[] args) {
-	System.out.println("IP: " + args[0]);
-	System.out.println("Puerto inicial: " + args[1]);
-	System.out.println("Puerto final: " + args[2]);
-
-      String servidor = args[0];
-      int puerto = 5302;
-
-	int puerto_inicial = Integer.parseInt(args[1]);
-	int puerto_final = Integer.parseInt(args[2]);
-
+      String servidor = "127.0.0.1";
+      int puerto = 5000;
       try{
-	System.out.println("Antes del socket");
         Socket socket= new Socket (servidor,puerto);
-	System.out.println("Después del socket");
+        BufferedReader in = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+
+	String c = "Saludos desde el cliente";
+        out.println(c);
+	String line = "";
+	while  ((line = in.readLine()) != null){
+		System.out.println(line);
+		break;
+	}
 	socket.close();
       } catch (IOException e)
       {
-       		System.out.println("Error en conexión al intentar con el puerto " + puerto  + "!!!");
+       		System.out.println("Error en conexión!!!");
       }
       
     }
